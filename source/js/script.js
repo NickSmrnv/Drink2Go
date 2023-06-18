109acce7d67cf1bc0f68ccf620e83805f2a09afc
Nikita <vinsmir@mail.ru>
2023-06-18 15:27:23 +0300

    feat(index.html, script.js): Подключение и настройка leaflet.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,25 @@
 
 
+// Интерактивная карта leaflet.js
 
+const map = L.map('map').setView([59.968389, 30.317533], 18);
+
+const myIcon = L.icon({
+  iconUrl: 'img/map-pin.png',
+  iconSize: [38, 50], // устанавливаем новый размер иконки
+  iconAnchor: [25, 25],
+  popupAnchor:  [0, -25]
+});
+
+const marker = L.marker([59.968389, 30.317533],{icon: myIcon}).addTo(map);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+map.zoomControl.remove(); // убираем обе кнопки
+map.panTo(marker.getLatLng()); // центрирование на маркере
 
 // Слайдер swiper.js
 
